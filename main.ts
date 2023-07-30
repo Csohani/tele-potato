a1847f5b6925f74e5df23a850b076c41ae70b501
Csohani <135929379+Csohani@users.noreply.github.com>
2023-07-30 18:36:48 +0200

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,12 @@
+let lista: number[][] = []
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (lista[randint(0, 3)]))
+})
+basic.forever(function () {
+    lista = [[
+    0,
+    1,
+    2,
+    6
+    ]]
+})
